458ee0d320fd53e672c26008ee0568d007815814
manishyml <manish.singh@ymedialabs.com>
2018-10-09 19:07:01 +0530

    changed env port

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 app.use("/users", users);
 
 //Setting port
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("Server running at", port);
