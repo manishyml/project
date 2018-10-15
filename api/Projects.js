851e291d8f3a30516b9a021d9a43d59a122a5455
manishyml <manish.singh@ymedialabs.com>
2018-10-15 12:25:28 +0530

    403 status removed

--- api/Projects.js
+++ api/Projects.js
@@ -7,14 +7,21 @@ router.get("/test", (req, res) => {
 });
 
 router.post("/new", (req, res) => {
-  const newUser = new Projects(req.body);
+  const newUser = new Projects({
+    Project_Id: req.body.Project_Id,
+    Project_Name: req.body.Project_Name,
+    Dealership: req.body.Dealership,
+    Deal_Amount: req.body.Deal_Amount,
+    Description: req.body.Description,
+    Team: req.body.Team
+  });
   newUser
     .save()
     .then(user => {
       res.json(user);
       res.send("Data Added To Database");
     })
-    .catch(err => res.sendStatus(403).json({ message: err }));
+    .catch(err => res.json({ message: err }));
 });
 router.get("/new", (req, res) => {
   Projects.find({}).then(users => {
